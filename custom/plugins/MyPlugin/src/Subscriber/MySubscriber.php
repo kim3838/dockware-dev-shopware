927fb64e6ed87bdd77086a4e26cacb1b3c7fd797
Kim@PC <kimdeguzman20@yahoo.com>
2024-10-16 21:14:39 +0800

    Debug Config and Product Minimal Off-canvas Cross Selling Group Index via Product Loaded Subscriber

--- custom/plugins/MyPlugin/src/Subscriber/MySubscriber.php
+++ custom/plugins/MyPlugin/src/Subscriber/MySubscriber.php
@@ -2,12 +2,25 @@
 
 namespace MyPlugin\Subscriber;
 
+use Psr\Log\LoggerInterface;
 use Shopware\Core\Framework\DataAbstractionLayer\Event\EntityLoadedEvent;
+use Shopware\Core\System\SystemConfig\SystemConfigService;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Shopware\Core\Content\Product\ProductEvents;
 
 class MySubscriber implements EventSubscriberInterface
 {
+    private SystemConfigService $systemConfigService;
+    private LoggerInterface $logger;
+
+    public function __construct(
+        SystemConfigService $systemConfigService,
+        LoggerInterface $logger
+    ){
+        $this->systemConfigService = $systemConfigService;
+        $this->logger = $logger;
+    }
+
     public static function getSubscribedEvents(): array
     {
         // Return the events to listen to as array like this:  <event to listen to> => <method to execute>
@@ -18,7 +31,18 @@ class MySubscriber implements EventSubscriberInterface
 
     public function onProductsLoaded(EntityLoadedEvent $event)
     {
-        // Do something
-        // E.g. work with the loaded entities: $event->getEntities()
+        $configCrossSellingGroupIndex = $this->systemConfigService->get('MyPlugin.config.minimalOffcanvasCrossSellingGroupIndex');
+
+        $productCustomFieldCrossSellingGroupIndex = $event->getEntities()[0]->getVars()['customFields']['custom_minimal_offcanvas_cross_selling_group_index_'] ?? null;
+
+        $minimalOffCanvasCrossSellingGroupIndex = empty($productCustomFieldCrossSellingGroupIndex)
+            ? $configCrossSellingGroupIndex
+            : $productCustomFieldCrossSellingGroupIndex;
+
+        $minimalOffCanvasCrossSellingGroupIndex = empty($minimalOffCanvasCrossSellingGroupIndex) ? 1 : $minimalOffCanvasCrossSellingGroupIndex;
+
+        $this->logger->debug('Config Cross Selling Group Index: ' . $configCrossSellingGroupIndex);
+        $this->logger->debug('Product Custom Field Cross Selling Group Index: ' . $productCustomFieldCrossSellingGroupIndex);
+        $this->logger->debug('Minimal Off-canvas Cross Selling Group Index: ' . $minimalOffCanvasCrossSellingGroupIndex);
     }
 }
